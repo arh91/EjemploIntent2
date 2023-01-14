28e0b84aac98352aab5890b153df623198ab16d2
arh91 <alvaro_rh_991@hotmail.com>
2023-01-14 19:51:26 +0100

    Aplicación con dos pantallas. Al hacer click en el botón de la primera pantalla, la app nos dirigirá a la segunda, pero además se cambiará la cadena de texto de la primera pantalla, y se enviarán un par de datos de la primera a la segunda pantalla

---
+++ app/src/main/java/com/example/ejemplointent2/MainActivity.kt
@@ -0,0 +1,62 @@
+package com.dam2.ejemplosintent
+
+import android.app.Activity
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import com.example.ejemplointent2.R
+
+class MainActivity : AppCompatActivity() {
+
+    // definir el requestCode
+    val RESULTADO_UNO=1
+    val RESULTADO_DOS=2
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val gosecond = findViewById<Button>(R.id.askMeBtn)
+
+        gosecond.setOnClickListener{
+            // Crea un Intent para iniciar la segunda actividad
+            val intent = Intent(this, SecondActivity::class.java)
+
+// Añade datos adicionales al Intent
+            intent.putExtra("proveedor", "Castelao")
+            intent.putExtra("cliente", "Google")
+
+// Inicia la segunda actividad
+            startActivityForResult(intent, RESULTADO_UNO)
+
+            startActivityForResult(intent, RESULTADO_DOS)
+        }
+
+    }
+
+    @Deprecated("Deprecated in Java")
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        val saludo = findViewById<TextView>(R.id.originalTextView)
+        val goSecond = findViewById<TextView>(R.id.textViewGo)
+        if(resultCode != Activity.RESULT_OK) return
+        when(requestCode) {
+            RESULTADO_UNO -> {
+                if (data != null) {
+                    saludo.text = data.getStringExtra("pregunta")
+                }; }
+            // Other result codes
+            else -> {}
+        }
+        when(requestCode) {
+            RESULTADO_DOS -> {
+                if (data != null) {
+                    goSecond.text = data.getStringExtra("hecho")
+                }; }
+            // Other result codes
+            else -> {}
+        }
+    }
+}
